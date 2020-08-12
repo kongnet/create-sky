03b86ec480ea6e49c875d3dc09ea81935fbb8e2c
kongnet <9601698@qq.com>
2020-08-12 23:40:35 +0800

    :art:

--- template/db-report/output-file.js
+++ template/db-report/output-file.js
@@ -3,7 +3,7 @@ const dir = __dirname
 function genConfig (obj) {
   let o = {
     'package.json': [path.join(dir, 'package.json'), obj, 'package.json'],
-    '.gitignore': [path.join(dir, '.gitignore'), obj, 'Git忽略文件列表'],
+    '.gitignore': [path.join(dir, '.npmignore'), obj, 'Git忽略文件列表'],
     '.editorconfig': [path.join(dir, '.editorconfig'), obj, 'editorconfig'],
     '.eslintignore': [path.join(dir, '.eslintignore'), obj, 'eslintignore'],
     'index.js': [path.join(dir, 'index.js'), obj, 'index'],
