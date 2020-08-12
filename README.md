6a037e29665dc3fd677ddf9695822d2e3cf9501a
kongnet <9601698@qq.com>
2020-08-12 23:45:25 +0800

    :memo:

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # create-sky
+
+### Mysql 数据结构输出模板
+
+> 问答如下
+
+- ? 请选择工程模板 > Choice your Template: DB-report > Mysql 库表结构生成
+- ? .项目路径,相对当前 [.]: .
+- ? Mysql host [127.0.0.1]: 127.0.0.1
+- ? Mysql port [3306]: 3306
+- ? Mysql user [root]: root
+- ? Mysql password [123456]: [hidden]
+- ? Mysql database [test]: test
--- template/db-report/output-file.js
+++ template/db-report/output-file.js
@@ -3,7 +3,7 @@ const dir = __dirname
 function genConfig (obj) {
   let o = {
     'package.json': [path.join(dir, 'package.json'), obj, 'package.json'],
-    '.gitignore': [path.join(dir, '.npmignore'), obj, 'Git忽略文件列表'],
+    '.gitignore': [path.join(dir, '.npmignore'), obj, 'Git忽略文件列表'], //注意这里 gitignore 会转成 npmignore放到临时目录，copy时需要转回来
     '.editorconfig': [path.join(dir, '.editorconfig'), obj, 'editorconfig'],
     '.eslintignore': [path.join(dir, '.eslintignore'), obj, 'eslintignore'],
     'index.js': [path.join(dir, 'index.js'), obj, 'index'],
