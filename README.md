a58e09d323267138d5f66399bd7a37f437993f18
kongnet <9601698@qq.com>
2021-01-13 10:26:44 +0800

    :memo:

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 > 问答如下
 
-- ? 请选择工程模板 > Choice your Template: DB-report > Mysql 库表结构生成
-- ? .项目路径,相对当前 [.]: .
-- ? Mysql host [127.0.0.1]: 127.0.0.1
-- ? Mysql port [3306]: 3306
-- ? Mysql user [root]: root
-- ? Mysql password [123456]: [hidden]
-- ? Mysql database [test]: test
+- 请选择工程模板 > Choice your Template: DB-report > Mysql 库表结构生成
+- .项目路径,相对当前 [.]: .
+- Mysql host [127.0.0.1]: 127.0.0.1
+- Mysql port [3306]: 3306
+- Mysql user [root]: root
+- Mysql password [123456]: [hidden]
+- Mysql database [test]: test
--- bin/create-sky.js
+++ bin/create-sky.js
@@ -11,19 +11,19 @@ const questionArr = [
     message: '请选择工程模板 > Choice your Template:',
     default: true,
     choices: [
-      { name: 'Github    > 常用Github项目生成(未加)', value: 1 },
-      { name: "Skybase   > Sky's web框架（未加）", value: 2 },
-      { name: 'DB-report > Mysql 库表结构生成', value: 3 }
+      { name: 'DB-report > Mysql 库表结构生成', value: 1 },
+      { name: 'Github    > 常用Github项目生成(未加)', value: 2 },
+      { name: "Skybase   > Sky's web框架（未加）", value: 3 }
     ]
   }
 ]
-async function main () {
+async function main() {
   let r = await inquirer.prompt(questionArr)
   //console.log(r)
   //console.log('PATH1:', __dirname)
   //console.log('PATH2:', process.cwd())
   switch (r.templateId) {
-    case 3:
+    case 1:
       require(path.join(__dirname, '../template/db-report/task.js'))
       break
   }
